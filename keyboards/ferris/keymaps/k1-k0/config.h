fd3b27020ac95faa4598f0296f3856a59050c83f
k1-k0 <kmssrvk@gmail.com>
2023-01-20 22:20:55 +0300

    return to default tapping term

--- keyboards/ferris/keymaps/k1-k0/config.h
+++ keyboards/ferris/keymaps/k1-k0/config.h
@@ -37,7 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 175
+#define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
