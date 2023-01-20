e0ad1f0cd96646478943eed3ff768b49e84020f2
k1-k0 <kmssrvk@gmail.com>
2023-01-20 22:03:40 +0300

    improve tap-hold features

--- keyboards/ferris/keymaps/k1-k0/config.h
+++ keyboards/ferris/keymaps/k1-k0/config.h
@@ -37,11 +37,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
+#define TAPPING_TERM 175
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
-// #define RETRO_TAPPING
+#define RETRO_TAPPING
 
 // Combo settings
 #define COMBO_TERM 20
