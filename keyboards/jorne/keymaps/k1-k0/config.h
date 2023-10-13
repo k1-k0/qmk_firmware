4d712c70076e5e063d4c1d62b828f878623b2f56
k1-k0 <kmssrvk@gmail.com>
2023-10-13 22:28:41 +0300

    fix jorne keymap and remove comments

--- keyboards/jorne/keymaps/k1-k0/config.h
+++ keyboards/jorne/keymaps/k1-k0/config.h
@@ -10,8 +10,7 @@
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_FORCE_HOLD
+#define QUICK_TAP_TERM 0
 
 
 #ifdef RGBLIGHT_ENABLE
