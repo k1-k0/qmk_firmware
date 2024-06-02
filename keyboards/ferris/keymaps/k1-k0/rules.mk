803b7480221e5e9e3b7bf4112078ca7addc13b7b
k1-k0 <kmssrvk@gmail.com>
2024-06-02 14:03:36 +0300

    update ferris tab key and enable repeat feature

--- keyboards/ferris/keymaps/k1-k0/rules.mk
+++ keyboards/ferris/keymaps/k1-k0/rules.mk
@@ -1,4 +1,5 @@
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-COMBO_ENABLE 	 = yes
-KEY_LOCK_ENABLE  = yes
-TAP_DANCE_ENABLE = no
+BOOTMAGIC_ENABLE   = yes      # Enable Bootmagic Lite
+COMBO_ENABLE 	   = yes
+KEY_LOCK_ENABLE    = yes
+REPEAT_KEY_ENABLE  = yes
+TAP_DANCE_ENABLE   = no
